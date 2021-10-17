97a8d6acd6617e1fbd09b6f44e137e018ab28144
NPBeta <shanhang007@Gmail.com>
2021-10-17 15:08:51 +0800

    Code Tweak

--- src/main/kotlin/com/npbeta/colorQPay/payments/alipay/AliPayCall.kt
+++ src/main/kotlin/com/npbeta/colorQPay/payments/alipay/AliPayCall.kt
@@ -12,7 +12,7 @@ class AliPayCall(
     val group: Long,
     val user: Long,
     private val callback: AliPayCallBack,
-) {
+): Runnable {
 
     private val maxTime = 300   // 5m
     lateinit var orderId: String
@@ -20,45 +20,45 @@ class AliPayCall(
     private var loadSuccess = false
     private lateinit var response: PayResponse
 
-    init {
-        object : Runnable {
+    override fun run() {
+        Main.logger.info("开始创建订单")
+        response = PayAPI.native("喵喵舰长服充值", price)
+        if (response.codeUrl == null) {
+            Main.logger.info("订单创建失败")
+            Main.sendGroupPrivateMessage(group, user, "订单创建失败，请重试")
+            return
+        }
+        orderId = response.orderId
+        Main.logger.info("订单 $orderId 已创建")
+        qrCodeImage = QRCodeHelper(response.codeUrl).image
+        class QueryTask : TimerTask() {
+
+            private var paySuccess = false
+            var timer = 0
+
             override fun run() {
-                response = PayAPI.native("喵喵舰长服充值", price)
-                if (response.codeUrl == null) {
-                    Main.sendGroupPrivateMessage(group, user, "订单创建失败，请重试")
-                    return
+                if (timer++ > maxTime) {
+                    callback.addLogs(user.toString(), price, "支付超时", this@AliPayCall)
+                    cancel()
                 }
-                orderId = response.orderId
-                qrCodeImage = QRCodeHelper(response.codeUrl).image
-                class QueryTask : TimerTask() {
-
-                    private var paySuccess = false
-                    var timer = 0
-
-                    override fun run() {
-                        if (timer++ > maxTime) {
-                            AliPayCallBack.addLogs(user.toString(), price, "支付超时", this@AliPayCall)
-                            cancel()
-                        }
-                        if (PayAPI.query(orderId) && !paySuccess) {
-                            paySuccess = true
-                            Main.logger.info("$user 支付 $price 成功")
-                            Main.sendGroupPrivateMessage(group, user, "支付成功，正在提交充值")
-                            if (AliPayCallBack.charge(price, user, this@AliPayCall)) {
-                                Main.logger.info("$user 充值 $price 成功")
-                                Main.sendGroupPrivateMessage(group, user, "充值成功")
-                            } else {
-                                Main.logger.warn("$user 充值 $price 失败")
-                                Main.sendGroupPrivateMessage(group, user, "充值失败，请联系服主")
-                            }
-                            cancel()
-                        }
+                if (PayAPI.query(orderId) && !paySuccess) {
+                    paySuccess = true
+                    Main.logger.info("$user 支付 $price 成功")
+                    Main.sendGroupPrivateMessage(group, user, "支付成功，正在提交充值")
+                    if (callback.charge(price, user, this@AliPayCall)) {
+                        Main.logger.info("$user 充值 $price 成功")
+                        Main.sendGroupPrivateMessage(group, user, "充值成功")
+                    } else {
+                        Main.logger.warn("$user 充值 $price 失败")
+                        Main.sendGroupPrivateMessage(group, user, "充值失败，请联系服主")
                     }
+                    cancel()
                 }
-                Timer().schedule(QueryTask(), Date(), 2000)
-                loadSuccess = true
             }
         }
+        Main.logger.debug("启动支付结果轮询")
+        Timer().schedule(QueryTask(), Date(), 2000)
+        loadSuccess = true
     }
 
     /**
