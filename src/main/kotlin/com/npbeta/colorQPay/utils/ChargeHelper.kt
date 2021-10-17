741d13fddf3637d5bdb5dcf7c680b79bf6c04a72
NPBeta <shanhang007@Gmail.com>
2021-10-17 15:09:57 +0800

    Fix not start again; Fix file not creating

--- src/main/kotlin/com/npbeta/colorQPay/utils/ChargeHelper.kt
+++ src/main/kotlin/com/npbeta/colorQPay/utils/ChargeHelper.kt
@@ -15,10 +15,13 @@ class ChargeHelper(
     override fun run() {
         Main.logger.info("正在调起支付")
         val payTask = AliPayCall(price, group, user, AliPayCallBack)
+        Thread(payTask).start()
         payTask.sendQRCode {
             Main.logger.info("正在发送二维码")
             val imagePath = "${Main.RunDir}temp/${payTask.orderId}.png"
-            ImageIO.write(payTask.qrCodeImage, "png", File(imagePath))
+            val imageFile = File(imagePath)
+            File(imagePath).createNewFile()
+            ImageIO.write(payTask.qrCodeImage, "png", imageFile)
             Main.sendGroupPrivateImage(group, user, imagePath)
             Main.sendGroupPrivateMessage(group, user, "请在 5 分钟内扫码并支付")
         }
