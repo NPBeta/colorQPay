c5cd13d71aa4544ba9f5140e4272ade06dd0cfa0
NPBeta <shanhang007@Gmail.com>
2021-10-17 17:01:34 +0800

    Cleanup

--- src/main/kotlin/com/npbeta/colorQPay/payments/alipay/AliPayCallBack.kt
+++ src/main/kotlin/com/npbeta/colorQPay/payments/alipay/AliPayCallBack.kt
@@ -3,12 +3,9 @@ package com.npbeta.colorQPay.payments.alipay
 import com.npbeta.colorQPay.Main
 import com.npbeta.colorQPay.utils.MCPanelAPIHelper
 import com.npbeta.colorQPay.utils.MySQLHelper
-import java.text.SimpleDateFormat
 
 
 object AliPayCallBack {
-
-    private val dateFormat = SimpleDateFormat("yyyy-MM-dd,hh:mm:ss")
 
     fun charge(price: Double, user: Long, payment: AliPayCall): Boolean {
         addLogs(user.toString(), price, "支付完成", payment)
