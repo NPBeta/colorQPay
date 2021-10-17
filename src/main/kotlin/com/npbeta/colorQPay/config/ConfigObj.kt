ce99d4c6296b023b959319ab77216f4945dcf19a
BuildTools <admin@npbeta.com>
2021-10-17 12:42:06 +0800

    Add config

--- src/main/kotlin/com/npbeta/colorQPay/config/ConfigObj.kt
+++ src/main/kotlin/com/npbeta/colorQPay/config/ConfigObj.kt
@@ -3,21 +3,38 @@ package com.npbeta.colorQPay.config
 class ConfigObj {
     var ip: String
     var port: Int
+    var qq: Long
     var group: List<Long>
     var prefix: String
     var chargeMultiplier: Int
+    var chargeCommand: String
     var appid: String
     var privateKey: String
     var alipayPublicKey: String
+    var mcPanelAPIURL: String
+    var mcPanelAPIKey: String
+    var mysqlAddress: String
+    var mysqlUsername: String
+    var mysqlPassword: String
+    var mysqlDatabase: String
+
 
     init {
         ip = "127.0.0.1"
         port = 23333
+        qq = 0
         group = listOf()
         prefix = ".充值"
         chargeMultiplier = 100
+        chargeCommand = "money give"
         appid = ""
         privateKey = ""
         alipayPublicKey = ""
+        mcPanelAPIURL = ""
+        mcPanelAPIKey = "apikey"
+        mysqlAddress = "127.0.0.1:3306"
+        mysqlUsername = "root"
+        mysqlPassword = ""
+        mysqlDatabase = "colorQPay"
     }
 }
--- src/main/kotlin/com/npbeta/colorQPay/config/ConfigUtils.kt
+++ src/main/kotlin/com/npbeta/colorQPay/config/ConfigUtils.kt
@@ -10,28 +10,32 @@ class ConfigUtils {
 
     private lateinit var configFile: File
 
+    fun parseFile(configFile: File): String {
+        val reader = InputStreamReader(
+            FileInputStream(configFile), StandardCharsets.UTF_8
+        )
+        val bf = BufferedReader(reader)
+        val buf = CharArray(4096)
+        var length: Int
+        val data = StringBuilder()
+        while (bf.read(buf).also { length = it } != -1) {
+            data.append(String(buf, 0, length))
+        }
+        bf.close()
+        reader.close()
+        return data.toString()
+    }
+
     fun read(local: String): Boolean {
         try {
-            configFile = File(local + "Config.json")
+            configFile = File("${local}Config.json")
             if (!configFile.exists()) {
                 configFile.createNewFile()
                 Main.Config = ConfigObj()
                 save()
                 return true
             } else {
-                val reader = InputStreamReader(
-                    FileInputStream(configFile), StandardCharsets.UTF_8
-                )
-                val bf = BufferedReader(reader)
-                val buf = CharArray(4096)
-                var length: Int
-                val data = StringBuilder()
-                while (bf.read(buf).also { length = it } != -1) {
-                    data.append(String(buf, 0, length))
-                }
-                Main.Config = JSON.parseObject(data.toString(), ConfigObj::class.java)
-                bf.close()
-                reader.close()
+                Main.Config = JSON.parseObject(parseFile(configFile), ConfigObj::class.java)
             }
         } catch (e: IOException) {
             e.printStackTrace()
